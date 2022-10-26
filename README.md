6807a198998d3874937ac59d72885e50a50d3ae7
JanusZhu <janus.zhu@yale.edu>
2022-10-27 01:07:07 +0900

    Refactored using class

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ A library where users can add, delete and create books.
 
 Using JavaScript object oriented programming, html css form and table elements.
 
+Updated 10/26/22:
+
+Refactored using class instead of pure constructors
+
 Inspirations from the Odin Project
 
 Live demo: https://januszhu.github.io/library/
--- script.js
+++ script.js
@@ -1,72 +1,40 @@
-let myLibrary = [];
-let id = 0;
-const table = document.querySelector("table");
-const $name = document.querySelector("#name");
-const $pages = document.querySelector("#pages");
-const $status = document.querySelector("#status");
-const $author = document.querySelector("#author");
-const $form = document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(Boolean($status.value));
-  let book = new Book($name.value, $author.value, $pages.value, $status.value);
-  addBookToLibrary(book);
-  render();
-  clearForm();
-});
+class Library {
+  constructor() {
+    this.myList = [];
+    this.id = 0;
+  }
 
-function clearForm() {
-  $name.value = null;
-  $author.value = null;
-  $pages.value = null;
-  $status.value = null;
+  addBookToLibrary(book) {
+    this.myList.push(book);
+  }
+
+  increaseId() {
+    this.id += 1;
+  }
+
+  get books() {
+    return this.myList;
+  }
+
+  get currentId() {
+    this.increaseId();
+    return this.id - 1;
+  }
+
+  set books(newList) {
+    this.myList = newList;
+  }
 }
 
-function submitForm() {}
-
-function Book(name, author, pages, status) {
+class Book {
   // the constructor...
-  this.id = id;
-  id += 1;
-  this.name = name;
-  this.author = author;
-  this.pages = Number(pages);
-  this.status = Boolean(status);
-}
-
-function addBookToLibrary(book) {
-  myLibrary.push(book);
-}
-
-//add delete addEventListener
-function addDelete() {
-  const btns = Array.from(document.querySelectorAll(".delete"));
-
-  btns.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      console.log("clicked");
-      myLibrary = myLibrary.filter((book) => {
-        return book.id != button.id;
-      });
-      //after filter ender again
-      render();
-    });
-  });
-}
-
-function addToggle() {
-  const statusButtons = Array.from(document.querySelectorAll(".status"));
-
-  statusButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      for (let book of myLibrary) {
-        if (book.id == button.getAttribute("data-id")) {
-          book.status = !book.status;
-        }
-      }
-      //after filter ender again
-      render();
-    });
-  });
+  constructor(id, name, author, pages, status) {
+    this.id = id;
+    this.name = name;
+    this.author = author;
+    this.pages = Number(pages);
+    this.status = Boolean(status);
+  }
 }
 
 function render() {
@@ -78,8 +46,8 @@ function render() {
   <th>&nbsp</th>
 </tr>`;
 
-  for (let book of myLibrary) {
-    console.log(book);
+  for (let book of library.books) {
+    //console.log(book);
     result += `
       <tr>
         <td>${book.name}</td>
@@ -99,9 +67,80 @@ function render() {
   addToggle();
 }
 
-let book1 = new Book("Harry Potter", "JK Rolling", 493, true);
-let book2 = new Book("House of Dragon", "George Martin", "589", false);
-addBookToLibrary(book1);
-addBookToLibrary(book2);
+function clearForm() {
+  $name.value = null;
+  $author.value = null;
+  $pages.value = null;
+  $status.value = null;
+}
+
+//add delete addEventListener
+function addDelete() {
+  const btns = Array.from(document.querySelectorAll(".delete"));
+
+  btns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      console.log("clicked");
+      library.books = library.books.filter((book) => {
+        return book.id != button.id;
+      });
+      //after filter ender again
+      render();
+    });
+  });
+}
+
+function addToggle() {
+  const statusButtons = Array.from(document.querySelectorAll(".status"));
+
+  statusButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      for (let book of library.books) {
+        if (book.id == button.getAttribute("data-id")) {
+          book.status = !book.status;
+        }
+      }
+      //after filter ender again
+      render();
+    });
+  });
+}
+
+let library = new Library();
+const table = document.querySelector("table");
+const $name = document.querySelector("#name");
+const $pages = document.querySelector("#pages");
+const $status = document.querySelector("#status");
+const $author = document.querySelector("#author");
+const $form = document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(Boolean($status.value));
+  let book = new Book(
+    library.currentId,
+    $name.value,
+    $author.value,
+    $pages.value,
+    $status.value
+  );
+  library.addBookToLibrary(book);
+  render();
+  clearForm();
+});
+let book1 = new Book(
+  library.currentId,
+  "Harry Potter",
+  "JK Rolling",
+  493,
+  true
+);
+let book2 = new Book(
+  library.currentId,
+  "House of Dragon",
+  "George Martin",
+  "589",
+  false
+);
+library.addBookToLibrary(book1);
+library.addBookToLibrary(book2);
 
 render();
